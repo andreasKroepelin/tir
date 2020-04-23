6cb48f127723ee691ce0668bba7809fd792ff378
Andreas <andreas.kroepelin@uni-jena.de>
2020-04-23 18:41:00 +0200

    Format with cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use prettytable::{cell, format, row, Table};
 use regex::Regex;
 use structopt::StructOpt;
+use uom::fmt::DisplayStyle;
 use uom::si::f64::{Length, Time, Velocity};
 use uom::si::length::{kilometer, meter};
 use uom::si::time::{hour, minute, second};
-use uom::si::velocity::{kilometer_per_hour};
-use uom::fmt::DisplayStyle;
-use prettytable::{cell,row, format,Table};
+use uom::si::velocity::kilometer_per_hour;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name="Today I Ran", about="This tool provides you with basic information derived from the distance you ran and the time you needed. This currently contains your average velocity and estimated times for other distances.")]
+#[structopt(
+    name = "Today I Ran",
+    about = "This tool provides you with basic information derived from the distance you ran and the time you needed. This currently contains your average velocity and estimated times for other distances."
+)]
 struct CommandLineOptions {
     #[structopt(help = "the distance you ran today")]
     distance: String,
@@ -103,14 +106,32 @@ fn main() {
     if let Some(run) = Run::from_options(&options) {
         let vel_format = Velocity::format_args(kilometer_per_hour, DisplayStyle::Abbreviation);
 
-        println!("Your average velocity was {}.\n", vel_format.with(run.average_velocity()));
+        println!(
+            "Your average velocity was {}.\n",
+            vel_format.with(run.average_velocity())
+        );
 
         let distances = &[
-            SpecialDistance{ name: String::from("1 km"), distance: Length::new::<kilometer>(1.0) },
-            SpecialDistance{ name: String::from("5 km"), distance: Length::new::<kilometer>(5.0) },
-            SpecialDistance{ name: String::from("10 km"), distance: Length::new::<kilometer>(10.0) },
-            SpecialDistance{ name: String::from("half marathon"), distance: Length::new::<kilometer>(21.0975) },
-            SpecialDistance{ name: String::from("marathon"), distance: Length::new::<kilometer>(42.195) },
+            SpecialDistance {
+                name: String::from("1 km"),
+                distance: Length::new::<kilometer>(1.0),
+            },
+            SpecialDistance {
+                name: String::from("5 km"),
+                distance: Length::new::<kilometer>(5.0),
+            },
+            SpecialDistance {
+                name: String::from("10 km"),
+                distance: Length::new::<kilometer>(10.0),
+            },
+            SpecialDistance {
+                name: String::from("half marathon"),
+                distance: Length::new::<kilometer>(21.0975),
+            },
+            SpecialDistance {
+                name: String::from("marathon"),
+                distance: Length::new::<kilometer>(42.195),
+            },
         ];
 
         let mut table = Table::new();
@@ -118,13 +139,15 @@ fn main() {
         for distance in distances {
             table.add_row(row![
                 distance.name,
-                format!("{}", display_time(&run.time_for_distance(&distance.distance)))
+                format!(
+                    "{}",
+                    display_time(&run.time_for_distance(&distance.distance))
+                )
             ]);
         }
 
         println!("This is how long you would have needed for other distances:");
         table.printstd();
-
     } else {
         println!("Could not parse the given distance and time.");
     }

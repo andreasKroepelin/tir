a3ac3d494d7896c81808b0f4722e14cccd20827b
Andreas <andreas.kroepelin@uni-jena.de>
2020-04-23 13:58:41 +0200

    Add Run struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use structopt::StructOpt;
+use uom::si::f32::{Length, Time};
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 struct Options {
     distance: String,
     time: String,
 }
 
+struct Run {
+    distance: Length,
+    time: Time,
+}
+
+impl Run {
+    fn from_options(options: &Options) -> Self {
+        
+    }
+}
 
 fn main() {
     let options = Options::from_args();
+    dbg!(options);
 }
